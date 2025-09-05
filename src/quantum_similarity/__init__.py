7bd55173636c4992eef19e5239feb4e99550e133
Justin Miller <justinamiller@gmail.com>
2025-09-05 09:48:04 -0700

    Create __init__.py

---
+++ src/quantum_similarity/__init__.py
@@ -0,0 +1,3 @@
+from .swap_test import swap_test_similarity, build_swap_test_circuit
+from .encoding import vector_to_statevector, normalize_vector, next_power_of_two
+from .text_features import text_to_vector
